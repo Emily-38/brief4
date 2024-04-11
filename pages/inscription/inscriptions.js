dc10df9958f8a2c41683fa231b4ef6e60407c2e1
emily <martorana.emily38@gmail.com>
2024-04-11 19:10:36 +0200

    login and register

---
+++ pages/inscription/inscriptions.js
@@ -0,0 +1,27 @@
+async function inscription (){
+const email= document.querySelector('#email').value
+const password= document.querySelector('#password').value
+const firstname= document.querySelector('#firstname').value
+const lastname= document.querySelector('#lastname').value
+
+let user ={
+    email:email,
+    password:password,
+    firstname:firstname,
+    lastname:lastname
+}
+
+let request = {
+    method: 'POST',
+    headers: {
+        'Content-Type': 'application/json; charset=utf-8',
+    },
+    body: JSON.stringify(user),
+}
+    let apiRequest = await fetch('http://localhost:3000/register', request)
+    let response = await apiRequest
+
+    if (response.status === 200) {
+    window.location.href = '../login/acceuil.html'
+}
+}
